1a85471017a7a4e20fba58a74067ee351d19d4a2
TTProg120 <tuan7261980@gmail.com>
2023-11-20 22:04:20 +0800

    update1120

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,6 +1,8 @@
 const wrapper = document.querySelector('.cards-wrapper');
     const dots = document.querySelectorAll('.dot');
     let activeDotNum = 0;
+    const prevBtn = document.querySelector('.prev-btn');
+    const nextBtn = document.querySelector('.next-btn');
 
     dots.forEach((dot, idx) => {
         dot.setAttribute('data-num', idx);
@@ -15,6 +17,7 @@ const wrapper = document.querySelector('.cards-wrapper');
                 dots[activeDotNum].classList.remove('active');
                 dots[clickedDotNum].classList.add('active');
                 activeDotNum = clickedDotNum;
+                updatePrevNextBtnVisibility();
             }
         });
     });
@@ -26,6 +29,8 @@ const wrapper = document.querySelector('.cards-wrapper');
             let pixels = -displayArea * activeDotNum;
             wrapper.style.transform = 'translateX(' + pixels + 'px)';
             updateActiveDot();
+            prevBtn.style.opacity = '100%';
+            nextBtn.style.opacity = '0';
         }
     }
 
@@ -36,6 +41,8 @@ const wrapper = document.querySelector('.cards-wrapper');
             let pixels = -displayArea * activeDotNum;
             wrapper.style.transform = 'translateX(' + pixels + 'px)';
             updateActiveDot();
+            prevBtn.style.opacity = '0';
+            nextBtn.style.opacity = '100%';
         }
     }
 
@@ -46,4 +53,9 @@ const wrapper = document.querySelector('.cards-wrapper');
                 dot.classList.add('active');
             }
         });
+    }
+
+    function updatePrevNextBtnVisibility() {
+        prevBtn.style.opacity = activeDotNum === 0 ? '0' : '100%';
+        nextBtn.style.opacity = activeDotNum === 1 ? '0' : '100%';
     }
